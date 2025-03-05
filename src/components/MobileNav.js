c760341c6fe87c420ba62553df9b24c7d144169a
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-03-05 15:14:59 +0530

    mobile nav build

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -5,11 +5,12 @@ import { NavLink } from 'react-router-dom'
 const MobileNav = () => {
   return (
     <section className="lg:hidden h-14 bg-neutral-600 bg-opacity-75 fixed bottom-0 w-full">
-      <div className="flex justify-between items-center px-4">
+      <div className="flex justify-between items-center px-4 text-neutral-400 h-full">
         {mobileNav.map((nav, index) => (
           // Remove the `return` outside and directly return the JSX
           <NavLink key={nav.label+"mobilenavigation"}
-          className={({ isActive }) =>`px-3 flex h-full items-center flex-col justify-center`}
+          to={nav.href}
+          className={({ isActive }) =>`px-3 flex h-full items-center flex-col justify-center ${isActive && 'text-white' }`}
           >
             <div className="text-2xl">
               {nav.icon}
