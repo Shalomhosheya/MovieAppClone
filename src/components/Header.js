4acbe9f5e30864fb878c0f91a21de14922238d7c
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-03-05 14:31:24 +0530

    header build

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 import logo from '../assets/image5-removebg-preview.png';
 import UserIcon from '../assets/user2.png';
 import { IoSearchSharp } from "react-icons/io5";
 
 const Header = () => {
+  const [searchInput, setsearchInput] = useState('');
+  const navigate = useNavigate();
+
   const navigation = [
     { label: 'TV Shows', href: '/tv' },
     { label: 'Movies', href: '/movie' }
   ];
-
+  useEffect(() => {
+    navigate(`/search?q=${searchInput}`);
+  }, [searchInput])
   return (
     <header className="fixed top-0 w-full h-16 bg-neutral-600 bg-opacity-75">
       <div className="container mx-auto px-2 flex items-center h-full gap-1">
@@ -33,16 +39,19 @@ const Header = () => {
           ))}
         </nav>
         <div className="ml-auto flex items-center gap-4">
-          <form>
-            <input
+          <form className="flex items-center gap-2">
+          <input
             type="text"
             placeholder="Search here..."
-            className="bg-neutral-500 text-white px-4 py-2 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
-            />
+            className="bg-neutral-500 text-white px-4 py-1 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+            onChange={(e) => setsearchInput(e.target.value)}  
+            value={searchInput}
+          />
+
+          <button className="text-2x1  text-white">
+                <IoSearchSharp />
+          </button>
           </form>
-         <div className="text-2x1 text-white">
-          <IoSearchSharp/>
-         </div>
 
       <div className="w-15 h-14 rounded-full overflow-hidden cursor-pointer active:scale-50">
        <img 
@@ -50,8 +59,8 @@ const Header = () => {
         alt="User" 
         className="w-full h-full object-cover"
       />
-  </div>
-</div>
+      </div>
+     </div>
 
       </div>
     </header>
