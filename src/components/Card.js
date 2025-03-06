7949807b562d6304b16aa54852cc99d1fe98d449
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-03-07 02:20:27 +0530

    home build

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import moment from 'moment';
 
 const Card = ({ data,trending,index }) => {
   const imageURL = useSelector(state => state.movieData.imageURL);
@@ -10,11 +11,18 @@ const Card = ({ data,trending,index }) => {
      <div className='absolute top-0'>
      {
         trending && (
-          <div >
-            {index}Trending
+          <div className='py-2 px-4 backdrop-blur-3xl text-white bg-black/60 rounded-full text-sm overflow-hidden'>
+            # {index} Trending
           </div>
         )
      }
+     </div>
+     <div className='absolute bottom-0 py-2 px-4 backdrop-blur-3xl w-full  bg-black/70 text-white  p-2 overflow-hidden'>
+       <h2 className='text-ellipsis text-lg'>{data.title || data.name}</h2>
+       <div className='text-sm text-neutral-400 flex justify-between items-center'>
+        <p>{moment(data.release_date).format('MMMM Do YYYY')}</p>
+        <p className='bg-black px-1 rounded-full text-xs text-white'>Rating: {Number(data.vote_average).toFixed(1)}</p>
+       </div>
      </div>
     </div>
   );
